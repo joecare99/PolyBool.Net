8c3c066cd8f12c9811359696825ebb456f2f1ba9
Joe Care <Joe_care_99@yahoo.com>
2024-04-29 22:20:05 +0200

    PolyBool-Tests

--- PolyBool.NetTests/Logic/SegmentSelectorTests.cs
+++ PolyBool.NetTests/Logic/SegmentSelectorTests.cs
@@ -13,44 +13,31 @@ namespace PolyBool.Net.Logic.Tests
     [TestClass()]
     public class SegmentSelectorTests
     {
-        private IList<IPoint> Val2Pnts(IList<double> pvals)
-        {
-            var result = new List<IPoint>();
-            for (int i = 0; i < pvals.Count - 1; i += 2)
-            {
-                result.Add(Point.New((decimal)pvals[i], (decimal)pvals[i + 1]));
-            }
-            return result;
-        }
-
         [DataTestMethod()]
-        [DataRow(new double[] {  } ,0,"")]
-        [DataRow(new double[] { 0, 0 } ,1, "(0, 0)")]
-        [DataRow(new double[] { 0, 0, 8, 8 } ,2, "(0, 0), (8, 8)")]
-        [DataRow(new double[] { 0, 0, 16, 0, 8, 8 } ,3, "(0, 0), (16, 0), (8, 8)")]
+        [DataRow(new double[] { }, 0, "")]
+        [DataRow(new double[] { 0, 0 }, 1, "(0, 0)")]
+        [DataRow(new double[] { 0, 0, 8, 8 }, 2, "(0, 0), (8, 8)")]
+        [DataRow(new double[] { 0, 0, 16, 0, 8, 8 }, 3, "(0, 0), (16, 0), (8, 8)")]
         public void Val2PntsTest(double[] pvals, int ex, string sExp)
         {
-            var pnts = Val2Pnts(pvals);
+
+            var pnts = PolyBoolTestsHelpers.Val2Pnts(pvals);
             Assert.IsInstanceOfType(pnts, typeof(IList<IPoint>));
             Assert.AreEqual(ex, pnts.Count);
-            Assert.AreEqual(sExp, string.Join(", ",pnts));
+            Assert.AreEqual(sExp, string.Join(", ", pnts));
         }
 
         [DataTestMethod()]
         [DataRow(new object[] { new double[] { 0, 0, 16, 0, 8, 8 } },
                  new object[] { new double[] { 16, 6, 8, 14, 0, 6 } },
-                new object[] { new double[] { 16, 6, 10, 6, 16, 0, 0, 0, 6, 6, 0, 6 ,8,14} })]
+                new object[] { new double[] { 16, 6, 10, 6, 16, 0, 0, 0, 6, 6, 0, 6, 8, 14 } })]
         [DataRow(new object[] { new double[] { 0, 0, 16, 0, 8, 8 } },
                  new object[] { new double[] { 2, 1, 14, 1, 8, 7 } },
                 new object[] { new double[] { 16, 0, 0, 0, 8, 8 } })]
         public void UnionTest(object[] p1, object[] p2, object[] ex)
         {
-            var pg1 = Polygon.New([
-                Region.New(Val2Pnts(p1[0] as IList<double>)
-                                   )], false);
-            var pg2 = Polygon.New([
-                Region.New(Val2Pnts(p2[0] as IList<double>)
-                                   )], false);
+            var pg1 = p1.Val2Poly();
+            var pg2 = p2.Val2Poly();
             var unified = SegmentSelector.Union(pg1, pg2);
             Assert.IsInstanceOfType(unified, typeof(IPolygon));
             Assert.AreEqual(ex.Length, unified.Regions.Count);
@@ -58,11 +45,11 @@ namespace PolyBool.Net.Logic.Tests
             {
                 var region = unified.Regions[i];
                 Assert.IsInstanceOfType(region, typeof(IRegion));
-                Assert.AreEqual((ex[i] as IList<double>).Count, region.Points.Count*2);
-                for (int j = 0; j < (ex[i] as IList<double>).Count-1; j += 2)
+                Assert.AreEqual((ex[i] as IList<double>).Count, region.Points.Count * 2);
+                for (int j = 0; j < (ex[i] as IList<double>).Count - 1; j += 2)
                 {
-                    Assert.AreEqual((decimal)(ex[i] as IList<double>)[j], region.Points[j / 2].X,$"R[{i}].P[{j/2}].X");
-                    Assert.AreEqual((decimal)(ex[i] as IList<double>)[j + 1], region.Points[j / 2].Y, $"R[{i}].P[{j/2}].X");
+                    Assert.AreEqual((decimal)(ex[i] as IList<double>)[j], region.Points[j / 2].X, $"R[{i}].P[{j / 2}].X");
+                    Assert.AreEqual((decimal)(ex[i] as IList<double>)[j + 1], region.Points[j / 2].Y, $"R[{i}].P[{j / 2}].X");
                 }
             }
         }
@@ -76,12 +63,8 @@ namespace PolyBool.Net.Logic.Tests
             new object[] { new double[] { 14, 1, 2, 1, 8, 7 }, new double[] { 16, 0, 0, 0, 8, 8 } })]
         public void DifferenceTest(object[] p1, object[] p2, object[] ex)
         {
-            var pg1 = Polygon.New([
-                Region.New(Val2Pnts(p1[0] as IList<double>)
-                                   )], false);
-            var pg2 = Polygon.New([
-                Region.New(Val2Pnts(p2[0] as IList<double>)
-                                   )], false);
+            var pg1 = p1.Val2Poly();
+            var pg2 = p2.Val2Poly();
             var unified = SegmentSelector.Difference(pg1, pg2);
             Assert.IsInstanceOfType(unified, typeof(IPolygon));
             Assert.AreEqual(ex.Length, unified.Regions.Count);
