f521b6ddc183b2689ad776ba858cad578a1024b6
Joe Care <Joe_care_99@yahoo.com>
2024-05-01 21:05:04 +0200

    SegmentIntersecter.ArrRegion not needed

--- Polybool.Net/Logic/SegmentIntersecter.cs
+++ Polybool.Net/Logic/SegmentIntersecter.cs
@@ -7,33 +7,33 @@ namespace PolyBool.Net.Logic
   
         public class SegmentIntersecter : Intersecter
         {
-            public void AddRegion(IRegion region)
-            {
-                // regions are a list of points:
-                //  [ [0, 0], [100, 0], [50, 100] ]
-                // you can add multiple regions before running calculate
-                IPoint pt1;
-                IPoint pt2 = region.Points[region.Points.Count - 1];
-                for (int i = 0; i < region.Points.Count; i++)
-                {
-                    pt1 = pt2;
-                    pt2 = region.Points[i];
+            //public void AddRegion(IRegion region)
+            //{
+            //    // regions are a list of points:
+            //    //  [ [0, 0], [100, 0], [50, 100] ]
+            //    // you can add multiple regions before running calculate
+            //    IPoint pt1;
+            //    IPoint pt2 = region.Points[region.Points.Count - 1];
+            //    for (int i = 0; i < region.Points.Count; i++)
+            //    {
+            //        pt1 = pt2;
+            //        pt2 = region.Points[i];
 
-                    int forward = PointUtils.PointsCompare(pt1, pt2);
-                    if (forward == 0) // points are equal, so we have a zero-length segment
-                    {
-                        continue; // just skip it
-                    }
+            //        int forward = PointUtils.PointsCompare(pt1, pt2);
+            //        if (forward == 0) // points are equal, so we have a zero-length segment
+            //        {
+            //            continue; // just skip it
+            //        }
 
-                    EventAddSegment(
-                        SegmentNew(
-                            forward < 0 ? pt1 : pt2,
-                            forward < 0 ? pt2 : pt1
-                        ),
-                        true
-                    );
-                }
-            }
+            //        EventAddSegment(
+            //            SegmentNew(
+            //                forward < 0 ? pt1 : pt2,
+            //                forward < 0 ? pt2 : pt1
+            //            ),
+            //            true
+            //        );
+            //    }
+            //}
 
             internal IList<ISegment> Calculate(IList<ISegment> segments1, bool isInverted1, IList<ISegment> segments2, bool isInverted2)
             {
