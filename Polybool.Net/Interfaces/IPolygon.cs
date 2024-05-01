a45c7a065ac2e3e9f2bfae6fcc540b065f02424d
Joe Care <Joe_care_99@yahoo.com>
2024-05-01 09:43:00 +0200

    generic Interfaces loosen dependency to decimal

--- Polybool.Net/Interfaces/IPolygon.cs
+++ Polybool.Net/Interfaces/IPolygon.cs
@@ -7,3 +7,9 @@ public interface IPolygon
     bool Inverted { get; set; }
     IList<IRegion> Regions { get; set; }
 }
+
+public interface IPolygon<T> where T : struct
+{
+    bool Inverted { get; set; }
+    IList<IRegion<T>> Regions { get; set; }
+}
--- Polybool.Net/Objects/Epsilon.cs
+++ Polybool.Net/Objects/Epsilon.cs
@@ -4,4 +4,9 @@
     {
         public static decimal Eps { get; set; } = 0.00001m;
     }
+
+    public static class Epsilon<T> where T : struct
+    {
+        public static T Eps { get; set; } = (T)(object)0.00001;
+    }
 }
